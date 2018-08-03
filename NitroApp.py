894a71df4c5a69c84791aebc7cafab85f239d3f8
Wadood Daoud <wadooddaoud@gmail.com>
2018-08-03 15:47:00 +0000

    commenting code

--- NitroApp.py
+++ NitroApp.py
@@ -3,24 +3,27 @@
 #This is the library for  communicating with the IOT hub on Azure
 from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
 from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
+
+#Importing the hologram nova cloud library which allows us to leverage hologram's cellular network
 from Hologram.HologramCloud import HologramCloud
+
+#import the raspberry pi GPIO library which allows you to access the GPIO (General Input/Output) pins of the raspberry pi
 import RPi.GPIO as GPIO
+
+# These are imports for libraries that are virtually consistent across all python code. sys is to interact with the system, re is a library for regular expressions,
+# and numpy is for mathematical functions within, but not limited to, arrays
 import sys,re
 import math
 import numpy as np
-#Import the firmware code from the Adafruit BM280 temp and humidity sensor
+import time
 
-#import the telemetry class which allows communication with iothubclient
+#import the telemetry class which allows communication with iothubclient. Telemetry is basically the eyes and ears of the IoT device. We use the telemetry module to
+# connect to the IoT Hub
 from telemetry import Telemetry
 
 #Import the firmware code for the Adafruit MAX31855 Amplifier for the Thermocouple
 import Adafruit_MAX31855.MAX31855 as MAX31855
 
-#import smbus for use with the DHT20 sensor and setting up the bus with the address
-import smbus
-import time
-bus = smbus.SMBus(1)
-#SHT20 address, 0x40(64)
 
 #import MCP3008 (ADC) Library 
 import Adafruit_MCP3008
@@ -28,28 +31,21 @@ import Adafruit_MCP3008
 #importing the DHT11 sensor library
 import Adafruit_DHT
 
+# the first command creates an object which allows connection to the hologram cloud. The next command actually initalizes a connection to the hologram cloud.
 #hologram = HologramCloud(dict(), network='cellular')
 #result = hologram.network.connect()
 
 
 
 #Setting the variable for the max length of the arrays storing data
+# We only madethe am2302 humidity array to mitigate errors within the sensor readings
 MAX_ARRAY_LENGTH = 50
-
-#Setting arrays for the temporary storage of data values
-#thermocoupleTemperatureArray = []
-#sht20TemperatureArray = []
-#sht20HumidityArray = []
-#bme280TemperatureArray = []
-#bme280HumidityArray = []
 am2302HumidityArray = []
-#am2302TemperatureArray = []
-#bme280PressureArray = []
-#transducerPressureArray = []
 
 
   
-#Setting the variables needed for the Current sensor code
+#Setting the variables needed for the Current sensor code. A lot of these numbers correlate to math that was carried out to define the voltage divider and burden sensor to provide
+#accurate current sensor readings
 vRMS = 120.0
 offset = 2.5
 numTurns = 2000
@@ -78,15 +74,13 @@ CS_max  = 20
 DO_max  = 16
 max_sensor = MAX31855.MAX31855(CLK_max, CS_max, DO_max)
 
-
-
 #configure pin for Adafruit_DHT11 Sensor
 DHTpin = 4
 
 #pin number for LED 
 LED_PIN_ADDRESS = 17
 
-#Making a telemetry object which will allow this computer to communicate with IotHub
+#Making a telemetry object which will allow this computer to initialize a connections with the IotHub
 telemetry = Telemetry()
 
 MESSAGE_COUNT = 0
@@ -98,9 +92,11 @@ SEND_CALLBACKS = 0
 RECEIVE_CONTEXT = 0
 SEND_REPORTED_STATE_CALLBACKS = 0
 
-#set the connection string variable as the 2nd parameter that was passed into the application call (i.e. "python app.py  [connection string]"")
-CONNECTION_STRING_NITRO = 'HostName=GastNitroGenHub.azure-devices.net;DeviceId=NitroGenCellular;SharedAccessKey=F/cqWVnYF/k6hRQnxV/X7a9IP/FzF6ibV5HRSJ2PDAw='
-#CONNECTION_STRING_NITRO = 'HostName=JA-TestProject.azure-devices.net;DeviceId=EnduranceCellular;SharedAccessKey=56PL8iS1Pb2i5SE2Qke8CXXIRLgmCwJVHL+CLauuW1o='
+# These are the connection strings that allow for the IoT Device to be authenticated with the IoT Hub. The shared access key is critical to the IoT hub allowing a connection and can
+# be found on the IoT hub under the devices ---> "connection string primary". First is for the E7 device and the second is for the endurance unit.
+#CONNECTION_STRING_NITRO = 'HostName=GastNitroGenHub.azure-devices.net;DeviceId=NitroGenCellular;SharedAccessKey=F/cqWVnYF/k6hRQnxV/X7a9IP/FzF6ibV5HRSJ2PDAw='
+CONNECTION_STRING_NITRO = 'HostName=GastNitroGenHub.azure-devices.net;DeviceId=EnduranceCellular;SharedAccessKey=qFWvwLuI3TE0LZOH38pEus6U2JYwfAiAFt0qdOwGHxg='
+
 #Using the MQTT protocol for sending messages. 
 #It is a lightweight messaging protocol for small devices and sensors
 PROTOCOL = IoTHubTransportProvider.MQTT
@@ -111,9 +107,10 @@ GPIO.setup(LED_PIN_ADDRESS, GPIO.OUT)
 
 
 #this is the message text variable that gets sent to the IOT hub after being formatted with the respective variables
-MSG_TXT = "{\"deviceId\": \"NitroGenCellular\", \"nitroGeneration\": %f,\"timeEpoch\": %f, \"globalTimeOn\": %f,\"dutyCycle\": %f,\"compState\": %f ,\"thermocoupleTemperature\": %f,\"transducerPressure\": %f,\"am2302Temperature\": %f,\"am2302Humidity\": %f}"
+#MSG_TXT = "{\"deviceId\": \"NitroGenCellular\", \"nitroGeneration\": %f,\"timeEpoch\": %f, \"globalTimeOn\": %f,\"dutyCycle\": %f,\"compState\": %f ,\"thermocoupleTemperature\": %f,\"transducerPressure\": %f,\"am2302Temperature\": %f,\"am2302Humidity\": %f}"
+MSG_TXT = "{\"deviceId\": \"EnduranceCellular\", \"nitroGeneration\": %f,\"timeEpoch\": %f, \"globalTimeOn\": %f,\"dutyCycle\": %f,\"compState\": %f ,\"thermocoupleTemperature\": %f,\"transducerPressure\": %f,\"am2302Temperature\": %f,\"am2302Humidity\": %f}"
 
-#not too sure about this method yet. I think it recieves data from the IOT hub as a confirmation that the data was recieved
+#This method is from Azure and it is printed in the IoT hub terminal and drives the messages that are recieved back from the IoT Hub which confirms that a message was recieved.
 def receive_message_callback(message, counter):
     global RECEIVE_CALLBACKS
     message_buffer = message.get_bytearray()
@@ -128,7 +125,7 @@ def receive_message_callback(message, counter):
     print ( "    Total calls received: %d" % RECEIVE_CALLBACKS )
     return IoTHubMessageDispositionResult.ACCEPTED
 
-#Simple method for sending text to the terminal signifying that a confirmation was recieved with the data passed into it
+#Simple method for sending text to the terminal of the IoT Hub signifying that a confirmation was recieved at the IoT hub with the data passed into it
 def send_reported_state_callback(status_code, user_context):
     global SEND_REPORTED_STATE_CALLBACKS
     print ( "Confirmation for reported state received with:\nstatus_code = [%d]\ncontext = %s" % (status_code, user_context) )
@@ -149,7 +146,8 @@ def send_confirmation_callback(message, result, user_context):
     print ( "    Total calls confirmed: %d" % SEND_CALLBACKS )
     led_blink()
 
-
+# Initializes an IoT Hub client which we use to handle our messages. This client gives a direct connection to the IoT hub as it contains the connection string for the device in use and
+# the communication protocol to be used with the IoT Hub
 def iothub_client_init(CONNECTION_STRING_GIVEN, PROTOCOL):
     #prepare the iothub client (basically the iot hub on azure)
     client = IoTHubClient(CONNECTION_STRING_GIVEN,PROTOCOL)
@@ -160,6 +158,9 @@ def iothub_client_init(CONNECTION_STRING_GIVEN, PROTOCOL):
     client.set_message_callback(receive_message_callback, RECEIVE_CONTEXT)
     return client
 
+
+#Method that sends text to the IoT hub terminal to show the time between messages. This time is also another way that the IoT hub authenticats the messages as the messages should be coming from a
+# similar time zone
 def print_last_message_time(client):
     try:
         last_message = client.get_last_message_receive_time()
@@ -171,6 +172,7 @@ def print_last_message_time(client):
         else:
             print ( iothub_client_error )
 
+# Main method where all of the sensor methods are called and all of the messages are actually created and sent to the Iot Hub.
 def iothub_client_sample_run():
     led_blink()
     global compRunning
@@ -179,17 +181,15 @@ def iothub_client_sample_run():
     global am2302HumidityArray
     try:
         client_nitro = iothub_client_init(CONNECTION_STRING_NITRO, PROTOCOL)
-        #client_junair = iothub_client_init(CONNECTION_STRING_JUNAIR, PROTOCOL)
         if client_nitro.protocol == IoTHubTransportProvider.MQTT:
             print ( "IoTHubClient is reporting state" )
             reported_state = "{\"newState\":\"standBy\"}"
             client_nitro.send_reported_state(reported_state, len(reported_state), send_reported_state_callback, 0)
-            #client_junair.send_reported_state(reported_state, len(reported_state), send_reported_state_callback, 0)
-        telemetry.send_telemetry_data(parse_iot_hub_name,"success", "The connection to the IOT Hub has been established!!!...Boom")
+        telemetry.send_telemetry_data(parse_iot_hub_name,"success", "The connection to the IOT Hub has been established!")
         while True:
             global MESSAGE_COUNT, MESSAGE_SWITCH
             if MESSAGE_SWITCH:
-                print ( "IoTHubClient sending %d messages...boom" % MESSAGE_COUNT )
+                print ( "IoTHubClient sending %d messages" % MESSAGE_COUNT )
                 #reading data from sensors and formatting messages to be sent to client(iotHub)
                 thermocoupleTemperature = c_to_f(max_sensor.readTempC())
 		am2302Humidity,am2302Temperature = Adafruit_DHT.read_retry(DHTsensor,DHTpin)
@@ -198,16 +198,23 @@ def iothub_client_sample_run():
                 if len(am2302HumidityArray) > 100:
                     am2302HumidityArray = am2302HumidityArray[1:50]
 		am2302HumidityArray.append(am2302Humidity)
-		# added .0000002 to nitroGeneration because webapp doesnt update with zero
-                transducerPressure  = ((mcp.read_adc(0)-72)*150.0/595.2)+.000002
+		# added .0000001 to nitroGeneration because webapp doesnt update with zero
+                transducerPressure  = ((mcp.read_adc(0)-72)*150.0/595.2)+.000001
                 compState = CheckCompressorState()
                 dutyCycleArray.append(compState)
                 dutyCycle = calculateDutyCycle()*100
 		timeEpoch = int(time.time())
                 if len(dutyCycleArray) > 200:
                     dutyCycleArray = dutyCycleArray[1:150]
-                # added .0000002 to nitroGeneration because webapp doesnt update with zero
-                nitroGeneration = (globalTimeOn * .0047619047619048)+ .000002
+                    
+                # This code below takes the data obtained from the Excel sheet named "LR-AP067-078.xls" and correlates duty cycle to nitrogen generated per second by the E7   
+                averageDutyCycle = 0.32816666667
+                averageSecondsPer1scf = (averageDutyCycle * 60*60)/6
+                averageNitroGenPerSec = 1/averageSecondsPer1scf
+                # added .0000001 to nitroGeneration because webapp doesnt update with zero
+                nitroGeneration = (globalTimeOn * averageNitroGenPerSec)+ .000001
+                
+                #The next lines of code deal with adding the variables to the MSG_TXT varialbe above and sending that message to the IoT hub client specified earlier in the document.
                 msg_text_formatted = MSG_TXT %(nitroGeneration,timeEpoch,globalTimeOn,dutyCycle,compState,thermocoupleTemperature,transducerPressure,c_to_f(am2302Temperature),am2302Humidity)
                 print(msg_text_formatted)
                 message = IoTHubMessage(msg_text_formatted)
@@ -216,7 +223,7 @@ def iothub_client_sample_run():
                 message.correlation_id = "correlation_%d" % MESSAGE_COUNT
                 #mapping extra properties for the message. basically just adding a temperature warning
                 prop_map = message.properties()
-                prop_map.add("TemperatureAlert... ","true" if thermocoupleTemperature > 90.0 else "false")
+                prop_map.add("TemperatureAlert... ","true" if thermocoupleTemperature > 120.0 else "false")
 
                 #sending the message to the terminal from iotHub
                 client_nitro.send_event_async(message,send_confirmation_callback,MESSAGE_COUNT)
@@ -238,58 +245,6 @@ def iothub_client_sample_run():
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
         print(globalTimeOn)
-        
-#Method to get the temperature data from the DHT20 sensor
-def GetShtTemp_f():
-    global compRunning
-    global globalTimeOn
-    # Send temperature measurement command
-    #		0xF3(243)	NO HOLD master
-    bus.write_byte(0x40, 0xF3)
-    time.sleep(0.5)
-    shtData0 = bus.read_byte(0x40)
-    shtData1 = bus.read_byte(0x40)
-    # Convert the data
-    temp = shtData0 * 256 + shtData1
-    cTemp= -46.85 + ((temp * 175.72) / 65536.0)
-    fTemp = cTemp * 1.8 + 32
-    return fTemp
-
-#Method to get the temperature data from the DHT20 sensor
-def GetShtTemp_c():
-    global compRunning
-    global globalTimeOn
-    # Send temperature measurement command
-    #		0xF3(243)	NO HOLD master
-    bus.write_byte(0x40, 0xF3)
-    time.sleep(0.5)
-    shtData0 = bus.read_byte(0x40)
-    shtData1 = bus.read_byte(0x40)
-    # Convert the data
-    temp = shtData0 * 256 + shtData1
-    cTemp= -46.85 + ((temp * 175.72) / 65536.0)
-    return cTemp
-        
-#Method to get the humidity data from the DHT20 sensor
-def GetShtHumid():
-    global compRunning
-    global globalTimeOn
-    # SHT25 address, 0x40(64)
-    # Send humidity measurement command
-    #		0xF5(245)	NO HOLD master
-    bus.write_byte(0x40, 0xF5)
-    time.sleep(0.5)
-
-    # SHT25 address, 0x40(64)
-    # Read data back, 2 bytes
-    # Humidity MSB, Humidity LSB
-    shtData0 = bus.read_byte(0x40)
-    shtData1 = bus.read_byte(0x40)
-
-    # Convert the data
-    shtHumidity = shtData0 * 256 + shtData1
-    shtHumidity = -6 + ((shtHumidity * 125.0) / 65536.0)
-    return shtHumidity
 
 
 #method to get the LED to blink for a specified amount of time
@@ -297,7 +252,7 @@ def led_blink():
     global compRunning
     global globalTimeOn
     GPIO.output(LED_PIN_ADDRESS,GPIO.HIGH)
-    time.sleep(1) #the time that the LED stays lit for
+    time.sleep(1)
     GPIO.output(LED_PIN_ADDRESS, GPIO.LOW)
 
 #self explanatory method
@@ -309,7 +264,8 @@ def parse_iot_hub_name():
 def c_to_f(c):
         return c * 9.0 / 5.0 + 32.0
 
-    
+# This method checks the state of the compressor by checking if there is an amperage draw above 0.40. This value was chosen because the "home" state of the current transformer is about
+# 0.20 so anything about 0.40 should be on. The globalTimeOn varialbe this is used to calculate the nitrogen generation by the system is also updated in this method.
 def CheckCompressorState():
     global globalTimeOn
     global compRunning
@@ -322,14 +278,14 @@ def CheckCompressorState():
         iPrimary = (voltage/rBurden)*numTurns
         acc += (iPrimary*iPrimary)
     iRMS = math.sqrt(acc/numSamples)
-    if iRMS > .30 and compRunning == False:
+    if iRMS > .40 and compRunning == False:
         compRunning = True
         startTime = time.time()
-    elif iRMS > .30 and compRunning == True:
+    elif iRMS > .40 and compRunning == True:
         timeElapsed = time.time() - startTime
         startTime = time.time()
         globalTimeOn += timeElapsed
-    elif iRMS <.30 and compRunning == True:
+    elif iRMS <.40 and compRunning == True:
         timeElapsed = time.time() - startTime
         globalTimeOn += timeElapsed
         compRunning = False
@@ -338,6 +294,7 @@ def CheckCompressorState():
     else:
         return 0
 
+#calculates the duty cycle of the device by checking how many times the compressor was on within a run.
 def calculateDutyCycle():
     global dutyCycleArray
     y = np.array(dutyCycleArray)
